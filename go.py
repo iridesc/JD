10b718329894db5720ac300db4dd94edbe3a7498
Irid <lielang6@live.com>
2019-05-03 18:38:29 +0800

    ha

--- go.py
+++ go.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException,NoSuchElementException
-from UpdateTryActivity import UpdateTryActivity
-from UpdateTryActivity import bar
+from bs4 import BeautifulSoup
 import time
 from retry import retry
 import random
@@ -10,24 +9,86 @@ import json
 import sys
 from math import exp,pi
 import urllib.parse
-
-
 import requests
 
 
 
+def bar(n,l,long=50,done='=',head='>',blank='.'):
+    print('[{}]{}%'.format((int(n/l*long)*done+head+blank*long)[0:long],round(n/l*100,2),))
+    return n+1
 
-TEST=False
-ServerAddr='http://111.231.78.78:1231/api/'
-# ServerAddr='http://0.0.0.0:80/api/'
-MaxDriverCleanN = 20
-DriverCleanN=0
-LeastOnlineTime=12
-EachRequestShopAmount=50
-EachUpdateShopAmount=1
-BeanDataRecent=14
-TryDataGap=1
 
+def LoadConf():
+    global Conf
+    global TEST
+    global JDBeanModelON
+    global JDTryModelON
+    global ServerAddr
+    global MaxDriverCleanN
+    global DriverCleanN
+    global  LeastOnlineTime
+    global  EachRequestShopAmount
+    global  EachUpdateShopAmount
+    global  BeanDataRecent
+    global  TryDataGap
+    global  UserLoginStatusTestGap
+    global  BeanWaitTime
+
+    try:
+        # 尝试载入
+        Conf=json.load(open('data/conf.json'))
+
+        TEST=Conf['TEST']
+        JDTryModelON=['JDTryModelON']
+        JDBeanModelON=['JDBeanModelON']
+        ServerAddr=Conf['ServerAddr']
+        MaxDriverCleanN = Conf['MaxDriverCleanN']
+        DriverCleanN=Conf['DriverCleanN']
+        LeastOnlineTime=Conf['LeastOnlineTime']
+        EachRequestShopAmount=Conf['EachRequestShopAmount']
+        EachUpdateShopAmount=Conf['EachUpdateShopAmount']
+        BeanDataRecent=Conf['BeanDataRecent']
+        TryDataGap=Conf['TryDataGap']
+        UserLoginStatusTestGap=Conf['UserLoginStatusTestGap']
+        BeanWaitTime=Conf['BeanWaitTime']
+
+    except Exception as e:
+        Conf={
+            'TEST':False,
+            'JDTryModelON':True,
+            'JDBeanModelON':True,
+            'JDTryModelON':True,
+            'JDBeanModelON':True,
+            'ServerAddr':'http://111.231.78.78:1231/api/',
+            'MaxDriverCleanN':20,
+            'DriverCleanN':0,
+            'LeastOnlineTime':12,
+            'EachRequestShopAmount':50,
+            'EachUpdateShopAmount':1,
+            'BeanDataRecent':1,
+            'TryDataGap':1,
+            'UserLoginStatusTestGap':30,
+            'BeanWaitTime':1,
+        }
+
+        TEST=Conf['TEST']
+        JDTryModelON=['JDTryModelON']
+        JDBeanModelON=['JDBeanModelON']
+        ServerAddr=Conf['ServerAddr']
+        MaxDriverCleanN = Conf['MaxDriverCleanN']
+        DriverCleanN=Conf['DriverCleanN']
+        LeastOnlineTime=Conf['LeastOnlineTime']
+        EachRequestShopAmount=Conf['EachRequestShopAmount']
+        EachUpdateShopAmount=Conf['EachUpdateShopAmount']
+        BeanDataRecent=Conf['BeanDataRecent']
+        TryDataGap=Conf['TryDataGap']
+        UserLoginStatusTestGap=Conf['UserLoginStatusTestGap']
+        BeanWaitTime=Conf['BeanWaitTime']
+
+        json.dump(Conf,open('data/conf.json','w'),ensure_ascii=False,indent=2)
+        print(str(e))
+        print('Using default Conf !')
+        print(Conf)
 
 def get_driver(headless=True,nopic=True,nostyle=True):
     systemtype=sys.platform
@@ -90,6 +151,172 @@ def clean_driver(driver,keepcookie=True):
         DriverCleanN+=1
     return driver
 
+def UpdateTryActivity():
+    
+    def GetPageAmount():
+        @retry(tries=3, delay=1, backoff=2)
+        def getpageamount():
+            r = requests.get('https://try.jd.com/activity/getActivityList',timeout=10)
+            r.raise_for_status
+            listsoup = BeautifulSoup(r.text, 'html.parser')
+            pageamount = int(listsoup.find_all(
+                'span', {'class': 'fp-text'})[0].i.text)+1
+
+            return pageamount
+        
+        print('更新试用数据 ...',end=' ')
+        try:
+            pageamount=getpageamount()
+        except Exception as e:
+                print(' in {} .\n{}'.format('getpageamount',str(e)))
+                raise Exception
+        print(pageamount)
+        print('Done .')
+        return pageamount
+
+    def getActivityIdList(pageamount):
+        @retry(tries=15, delay=1, backoff=2)
+        def getListPageText(n):
+            r = requests.get(
+                'https://try.jd.com/activity/getActivityList?page={}&activityState=0'.format(n),timeout=10)
+            return r.text
+        
+        @retry(tries=5, delay=1, backoff=2)
+        def RemoveExistingActivityId(activity_id_list):
+            print('去除服务端已存在的ID ...' ,end=' ')
+            send_data={
+            'Reason':'RemoveExistingActivityId',
+            'ActivityIdList':activity_id_list,
+            }
+            r=requests.post(ServerAddr,json=send_data)
+            r.raise_for_status
+            data=r.json()
+            print(len(activity_id_list),' -> ',len(data['ActivityIdList']))
+            print('Done .')
+            return data['ActivityIdList']
+        
+        print('获取需要抓活动ID列表 ...')
+        n = 1
+        all_activity_id_list = []
+        while n <= pageamount:
+            n=bar(n,pageamount)
+            # 获取 activity_id
+            try:
+                text=getListPageText(n)
+            except Exception as e:
+                print('error in {} .\n{}'.format('getListPageText',str(e)))
+                continue
+            listsoup = BeautifulSoup(text, 'html.parser')
+            for li in listsoup.find('div', {'class': 'con'}).find_all('li'):
+                # 只获取24h内可以结束的
+                #if (int(li.attrs['end_time'])/1000-time.time())/(60*60) < 24:
+                all_activity_id_list.append(li.attrs['activity_id'])
+        
+        # 消除服务端已保存的的
+        try:
+            activity_id_list=RemoveExistingActivityId(all_activity_id_list)
+        except Exception as e:
+            activity_id_list = all_activity_id_list
+            print('error in {} .\n{}'.format('RemoveExistingActivityId',str(e)))
+
+        return activity_id_list
+
+    def getattrs(activity_id_list):
+
+        @retry(tries=5, delay=1, backoff=2)
+        def get_activity_data(activity_id):
+            url='https://try.jd.com/migrate/getActivityById?id={}'.format(activity_id)
+            #print(url)
+            r = requests.get(url,timeout=10).json()
+            data = r['data']
+            return data
+        
+        @retry(tries=5, delay=1, backoff=2)
+        def get_price(iteminfo):
+            url= 'https://p.3.cn/prices/mgets?skuIds=J_{}'.format(iteminfo['TrialSkuId'])
+            #print(url)
+            r=requests.get(url,timeout=10)
+            j=r.json()
+            return j[0]['p']
+
+
+
+        
+        print('获取活动详情 ...')
+        trydata=[]
+        n=0
+        l=len(activity_id_list)
+        for activity_id in activity_id_list:
+            n=bar(n,l)
+            iteminfo = {}
+        
+            # 获取各种属性
+            try:
+                data=get_activity_data(activity_id)
+            except Exception as e:
+                print('error in {} .\n{}'.format('get_activity_data',str(e)))
+                continue
+        
+
+            # 活动属性提取
+            iteminfo['ActivityId'] = activity_id
+            iteminfo['TrialSkuId'] = data['trialSkuId']
+            iteminfo['StartTime'] = data['startTime']/1000
+            iteminfo['EndTime'] = data['endTime']/1000
+            iteminfo['SupplyCount'] = data['supplyCount']
+            iteminfo['TrialName'] = data['trialName']
+            try:
+                iteminfo['ShopName'] = data['shopInfo']['title']
+                iteminfo['ShopId'] = data['shopInfo']['shopId']
+            except TypeError:
+                print('TypeError when get activity {} shop info '.format(
+                    iteminfo['ActivityId']))
+                iteminfo['ShopName'] = ''
+                iteminfo['ShopId'] = ''
+
+            # 获取价格
+            try:
+                price = get_price(iteminfo)
+            except Exception as e:
+                print('error in {} .\n{}'.format('get_price',str(e)))
+                price = 25
+            iteminfo['Price'] = float(price)
+
+            trydata.append(iteminfo)
+        
+        return trydata
+
+    def Update(try_activity_list):
+
+        @retry(tries=5, delay=1, backoff=2)    
+        def updatetrydata(try_activity_list):
+            send_data={
+
+                'Reason':'UpdateTryData'
+            }
+
+            send_data['TryActivityList']=try_activity_list
+            r=requests.post(ServerAddr,json=send_data)
+            # print(r.status_code)
+            r.raise_for_status
+            data=r.json()
+            return data
+        print('上传活动数据 ...')
+        try:
+            updatetrydata(try_activity_list)
+        except Exception as e:
+            print(' in {} .\n{}'.format('UpdateTryData',str(e)))
+        print('Done .')
+
+    # 获取页数
+    pageamount=GetPageAmount()
+    # 获取 activity_id_list
+    need_check_activity_id_list = getActivityIdList(pageamount)
+    # 获取活动信息
+    try_activity_list= getattrs(need_check_activity_id_list)
+    # 上传
+    Update(try_activity_list)
+
 def estimate(trydata):
     def loadrule():
         try:
@@ -100,9 +327,10 @@ def estimate(trydata):
                 '旗舰': 15,
                 '价格': 30,
                 '数量': 30,
-                '关键字': 20,
+                '优先': 20,
+                '排除': 30,
                 '优先关键字': ['鼠标', '键盘', '硬盘', '内存', '显卡', '笔记本', '中性笔', '路由器', '智能', 'u盘', '耳机', '音箱', '储存卡'],
-                '排除关键字': ['丝袜', '文胸', '舞鞋','课程', '流量卡', '婴儿', '手机壳','钢化膜', '润滑油', '纸尿裤','白酒', '药', '保健品'],
+                '排除关键字': ['丝袜', '文胸', '舞鞋','课程', '流量卡','手机卡', '婴儿', '手机壳','钢化膜', '润滑油', '纸尿裤','白酒', '药', '保健品'],
             }
             json.dump(rule, open('./data/rule.txt', 'w'),ensure_ascii=False,indent=4)
 
@@ -134,10 +362,10 @@ def estimate(trydata):
             score = 0
             for key in rule['优先关键字']:
                 if key in text:
-                    score += rule['关键字']
+                    score += rule['优先']
             for key in rule['排除关键字']:
                 if key in text:
-                    score -= rule['关键字']
+                    score -= rule['排除']
             return score
 
         scorelist = [
@@ -375,7 +603,7 @@ def jdtry(driver):
     def GetTryData(TryDataGap):
         send_data={
         'Reason':'GetTryData',
-        'Days':1,
+        'Days':TryDataGap,
         }
 
         r=requests.post(ServerAddr,json=send_data)
@@ -401,7 +629,7 @@ def jdtry(driver):
         else:
             print(data)
             raise Exception
-            
+
     print('Done .')
 
 
@@ -530,7 +758,7 @@ def jdbean(driver):
             
             # 等待获取优惠
             try:
-                btn = WebDriverWait(driver, 2).until(
+                btn = WebDriverWait(driver, BeanWaitTime).until(
                     lambda d: d.find_element_by_css_selector("[class='J_drawGift d-btn']"))
                 btn.click()
 
@@ -560,8 +788,14 @@ def jdbean(driver):
     return driver
 
 
+
+
+
 if __name__ == '__main__':
     try:
+        # 载入配置
+        LoadConf()
+
         # login
         driver = login()
     
@@ -569,10 +803,12 @@ if __name__ == '__main__':
         driver=delfollows(driver)
         
         # try items
-        driver=jdtry(driver)
+        if JDBeanModelON:
+            driver=jdtry(driver)
 
         # get bean
-        driver=jdbean(driver)
+        if JDBeanModelON:
+            driver=jdbean(driver)
 
         # quite
         driver.quit()
